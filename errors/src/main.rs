e5449794bbdd073d79cb823523c76ae08ff70db1
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-06-18 14:19:00 +0000

    add errors

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,10 +1,19 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
 
-fn results() {
-    let f = File::open("hello.txt");
+fn results_match() {
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt")
+                .unwrap_or_else(|error| panic!("Problem creating the file: {:?}", error))
+        } else {
+            panic!("Problem opening the file: {:?}", error)
+        }
+    });
+}
 
-    let f = match f {
+fn results_unwrap() {
+    let f = match File::open("hello.txt") {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
@@ -16,17 +25,38 @@ fn results() {
             }
         },
     };
+}
 
-    let f = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt")
-                .unwrap_or_else(|error| panic!("Problem creating the file: {:?}", error))
-        } else {
-            panic!("Problem opening the file: {:?}", error)
-        }
-    });
+fn results_expect() {
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+fn read_username_from_file_verbose() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_slim() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
 
 fn main() {
-    results();
+    results_expect();
 }
