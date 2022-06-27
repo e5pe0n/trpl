ab7422576c71368f0df36363a3ee9d9f28d72578
e5pe0n <1izard.synchr0fi5h@gmail.com>
2022-06-27 14:08:12 +0000

    impl iterators

---
+++ iterators/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    // for val in v1_iter {    // use of moved value: `v1_iter`
+    //     println!("Got: {}", val);
+    // }
+
+    let v1_iter = v1.iter();
+    let total: i32 = v1_iter.sum();
+    assert_eq!(total, 6);
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+}
